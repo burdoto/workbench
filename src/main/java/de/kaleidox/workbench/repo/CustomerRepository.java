e4e7f9ff41d71b7bd2fb012003f05a2e23de289b
Kaley <kaleidox@comroid.org>
2025-09-21 12:15:07 +0200

    add customer queries

--- src/main/java/de/kaleidox/workbench/repo/CustomerRepository.java
+++ src/main/java/de/kaleidox/workbench/repo/CustomerRepository.java
@@ -1,9 +1,29 @@
 package de.kaleidox.workbench.repo;
 
 import de.kaleidox.workbench.model.jpa.representant.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collection;
+
+@Controller
 @Repository
+@RequestMapping("/api/customers")
 public interface CustomerRepository extends CrudRepository<Customer, Customer.CompositeKey> {
+    @ResponseBody
+    @GetMapping("/names")
+    @Query("select distinct c.name from Customer c")
+    Collection<String> names();
+
+    @ResponseBody
+    @GetMapping("/{name}/departments")
+    @Query("select c.department from Customer c where c.name= :name")
+    Collection<String> departments(@PathVariable @Param("name") String name);
 }
