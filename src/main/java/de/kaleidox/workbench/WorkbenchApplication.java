cf8f8dd0e5a83bc91f0f7871868bf858b6d41071
kaleidox <github@kaleidox.de>
2025-09-24 19:42:14 +0200

    use different port on prod

--- src/main/java/de/kaleidox/workbench/WorkbenchApplication.java
+++ src/main/java/de/kaleidox/workbench/WorkbenchApplication.java
@@ -3,6 +3,7 @@ package de.kaleidox.workbench;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.kaleidox.workbench.model.config.AppConfig;
 import lombok.extern.java.Log;
+import org.comroid.api.func.util.Debug;
 import org.mariadb.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.annotation.Order;
@@ -27,7 +30,7 @@ import java.util.Objects;
 @EntityScan(basePackages = "de.kaleidox.workbench.model.jpa")
 @EnableJpaRepositories(basePackages = "de.kaleidox.workbench.repo")
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class WorkbenchApplication {
+public class WorkbenchApplication implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     public static void main(String[] args) {
         SpringApplication.run(WorkbenchApplication.class, args);
     }
@@ -62,5 +65,10 @@ public class WorkbenchApplication {
                 .password(db.password())
                 .driverClassName(Driver.class.getCanonicalName())
                 .build();
+    }
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(Debug.isDebug() ? 8080 : 42032);
     }
 }
